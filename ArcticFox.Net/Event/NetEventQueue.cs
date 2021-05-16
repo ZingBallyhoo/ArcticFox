13a282db7f7621959d865d46bfdbabbe3e10696f
zingballyhoo <zingballyhoo@live.co.uk>
2021-05-16 22:23:56 +0100

    net: use AsyncLockedAccess in NetEventQueue

--- ArcticFox.Net/Event/NetEventQueue.cs
+++ ArcticFox.Net/Event/NetEventQueue.cs
@@ -1,16 +1,15 @@
 using System;
 using System.Collections.Generic;
-using System.Threading;
 using System.Threading.Tasks;
 using ArcticFox.Net.Batching;
 using ArcticFox.Net.Sockets;
+using ArcticFox.Net.Util;
 
 namespace ArcticFox.Net.Event
 {
     public class NetEventQueue : IBroadcaster, IAsyncDisposable
     {
-        private readonly SemaphoreSlim m_eventQueueSema = new SemaphoreSlim(1, 1);
-        private readonly Queue<NetEvent> m_eventQueue = new Queue<NetEvent>();
+        private readonly AsyncLockedAccess<Queue<NetEvent>> m_eventQueue = new AsyncLockedAccess<Queue<NetEvent>>(new Queue<NetEvent>());
         private bool m_acceptingEvents = true;
         
         public int m_maxQueueSize = 100;
@@ -22,21 +21,18 @@ namespace ArcticFox.Net.Event
 
         private void EnqueueEvent(NetEvent @event)
         {
-            m_eventQueueSema.Wait();
-            try
+            using (var queueToken = m_eventQueue.GetSync())
             {
+                var queue = queueToken.m_value;
                 if (!m_acceptingEvents) return;
 
-                if (m_maxQueueSize != -1 && m_eventQueue.Count > m_maxQueueSize)
+                if (m_maxQueueSize != -1 && queue.Count > m_maxQueueSize)
                 {
                     // Log.Error("Send queue too big, dropping packet");
                     return;
                 }
                 @event.GetRef();
-                m_eventQueue.Enqueue(@event);
-            } finally
-            {
-                m_eventQueueSema.Release();
+                queue.Enqueue(@event);
             }
         }
 
@@ -44,45 +40,35 @@ namespace ArcticFox.Net.Event
         {
             if (socket.IsClosed()) return 0;
             
-            var gotLock = await m_eventQueueSema.WaitAsync(TimeSpan.Zero, default);
-            if (!gotLock) return 0;
-
-            int count;
-            try
+            using (var queueToken = m_eventQueue.GetNoWait())
             {
-                var q = m_eventQueue;
-                count = q.Count;
+                if (queueToken == null) return 0;
                 
-                while (q.Count > 0)
+                var queue = queueToken.m_value;
+                var count = queue.Count;
+                
+                while (queue.Count > 0)
                 {                    
-                    var ev = q.Dequeue();
-                    await ctx.AddMessage(socket, ev.GetMemory(), q.Count);
+                    var ev = queue.Dequeue();
+                    await ctx.AddMessage(socket, ev.GetMemory(), queue.Count);
                     ev.ReleaseRef();
                 }
                 await ctx.Flush(socket);
                 
                 return count;
-            } finally
-            {
-                m_eventQueueSema.Release();
             }
         }
         
         private async Task DisposePendingEvents()
         {
-            await m_eventQueueSema.WaitAsync();
-
-            try
+            using (var token = await m_eventQueue.Get())
             {
                 m_acceptingEvents = false;
 
-                while (m_eventQueue.TryDequeue(out var ev))
+                while (token.m_value.TryDequeue(out var ev))
                 {
                     ev.ReleaseRef();
                 }
-            } finally
-            {
-                m_eventQueueSema.Release();
             }
         }
         
--- ArcticFox.Net/Util/AsyncLockedAccess.cs
+++ ArcticFox.Net/Util/AsyncLockedAccess.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Threading;
 using System.Threading.Tasks;
 
@@ -15,9 +16,22 @@ namespace ArcticFox.Net.Util
 
         public ValueTask<AsyncLockToken<T>> Get()
         {
-            var waitTask =  m_sema.WaitAsync();
+            var waitTask = m_sema.WaitAsync();
             if (waitTask.IsCompleted) return new ValueTask<AsyncLockToken<T>>(m_token);
             return new ValueTask<AsyncLockToken<T>>(GetAwaited(waitTask)); // allocating path
+        }
+        
+        public AsyncLockToken<T>? GetNoWait()
+        {
+            var result = m_sema.Wait(TimeSpan.Zero);
+            if (result) return m_token;
+            return null;
+        }
+        
+        public AsyncLockToken<T> GetSync()
+        {
+            m_sema.Wait();
+            return m_token;
         }
 
         private async Task<AsyncLockToken<T>> GetAwaited(Task waitTask)
