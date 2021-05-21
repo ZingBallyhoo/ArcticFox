7d0277b3b4a10e1c45fddeaaf5ccca81d82d0902
zingballyhoo <zingballyhoo@live.co.uk>
2021-05-21 22:31:54 +0100

    net: allow pre-net transform again

--- ArcticFox.Net/HighLevelSocket.cs
+++ ArcticFox.Net/HighLevelSocket.cs
@@ -15,15 +15,31 @@ namespace ArcticFox.Net
         private readonly NetEventQueue m_netEventQueue;
 
         protected CodecChain? m_netInputCodec;
-        protected CodecChain<byte, byte>? m_preNetTransform;
+
+        private bool m_hasPreNetTransform;
+        protected AsyncLockedAccess<CodecChain<byte, byte>?> m_preNetTransform;
 
         public HighLevelSocket(SocketInterface socket)
         {
             m_socket = socket;
             m_taskQueue = new TaskQueue();
             m_netEventQueue = new NetEventQueue();
+            m_preNetTransform = new AsyncLockedAccess<CodecChain<byte, byte>?>(null);
         }
 
+        protected void SetPreNetTransform(CodecChain<byte, byte>? chain)
+        {
+            // todo: i guess its fine to block here
+            using var token = m_preNetTransform.GetSync();
+            m_hasPreNetTransform = chain != null;
+            var current = token.m_value;
+            token.m_value = chain;
+            if (current != null)
+            {
+                current.Dispose();
+            }
+        }
+        
         public virtual void NetworkInput(ReadOnlyMemory<byte> data)
         {
             var inputCodec = m_netInputCodec;
@@ -48,23 +64,33 @@ namespace ArcticFox.Net
         
         public ValueTask BroadcastEvent(NetEvent ev)
         {
-            //if (m_preNetTransform != null)
-            //{
-            //    lock (m_preNetTransform) // sanity: prevent stateful crypto out of order or corruption?
-            //    {
-            //        m_preNetTransform.Input2(ev.GetMemory().Span, m_netEventQueue);
-            //    }
-            //} else
-            //{
+            if (m_hasPreNetTransform)
+            {
+                return BroadcastWithTransform(ev);
+            } else
+            {
                 return m_netEventQueue.BroadcastEvent(ev);
-            //}
+            }
+        }
+
+        private async ValueTask BroadcastWithTransform(NetEvent ev)
+        {
+            using var transformToken = await m_preNetTransform.Get();
+            var transform = transformToken.m_value;
+            if (transform == null)
+            {
+                // shouldn't have gotten here, but lets handle it anyway
+                await m_netEventQueue.BroadcastEvent(ev);
+                return;
+            }
+            transform.Input2(ev.GetMemory(), m_netEventQueue);
         }
 
         public async ValueTask DisposeAsync()
         {
             await m_netEventQueue.DisposeAsync();
             m_netInputCodec?.Dispose();
-            m_preNetTransform?.Dispose();
+            SetPreNetTransform(null);
             m_socket.Dispose();
         }
     }
