29c3f7f84861ee7bccaa3e9184a7fb71abb54913
zingballyhoo <zingballyhoo@live.co.uk>
2021-12-30 05:30:08 +0000

    net: add StreamSocket

--- ArcticFox.Net/ClientSocketHostExtensions.cs
+++ ArcticFox.Net/ClientSocketHostExtensions.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Net;
+using System.Net.Security;
 using System.Net.Sockets;
 using System.Net.WebSockets;
 using System.Threading.Tasks;
@@ -26,6 +27,21 @@ namespace ArcticFox.Net
             await socket.ConnectAsync(endPoint);
             
             var tcpSocket = new TcpSocket(socket);
+            var hl = host.CreateHighLevelSocket(tcpSocket);
+            await host.AddSocket(hl);
+            return (T)hl;
+        }
+        
+        public static async ValueTask<T> CreateClientSslSocket<T>(this SocketHost host, DnsEndPoint endPoint) where T : HighLevelSocket
+        {
+            var socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            await socket.ConnectAsync(endPoint);
+
+            var netStream = new NetworkStream(socket);
+            var sslStream = new SslStream(netStream);
+            await sslStream.AuthenticateAsClientAsync(endPoint.Host);
+
+            var tcpSocket = new StreamSocket(sslStream);
             var hl = host.CreateHighLevelSocket(tcpSocket);
             await host.AddSocket(hl);
             return (T)hl;
---
+++ ArcticFox.Net/Sockets/StreamSocket.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace ArcticFox.Net.Sockets;
+
+public class StreamSocket : SocketInterface
+{
+    private readonly Stream m_stream;
+
+    public StreamSocket(Stream stream)
+    {
+        m_stream = stream;
+    }
+
+    public override ValueTask SendBuffer(ReadOnlyMemory<byte> data)
+    {
+        return m_stream.WriteAsync(data);
+    }
+
+    public override ValueTask<int> ReceiveBuffer(Memory<byte> buffer)
+    {
+        return m_stream.ReadAsync(buffer, m_cancellationTokenSource.Token);
+    }
+
+    protected override ValueTask CloseSocket()
+    {
+        m_stream.Close();
+        return ValueTask.CompletedTask;
+    }
+}
