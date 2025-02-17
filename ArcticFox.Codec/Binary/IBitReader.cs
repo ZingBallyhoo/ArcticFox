15fde549f1d13f385328de3848e3275135841fd6
zingballyhoo <zingballyhoo@live.co.uk>
2025-02-17 01:33:16 +0000

    remove blank line

--- ArcticFox.Codec/Binary/IBitReader.cs
+++ ArcticFox.Codec/Binary/IBitReader.cs
@@ -4,16 +4,15 @@ namespace ArcticFox.Codec.Binary
 {
     public partial interface IBitReader
     {
+        bool ReadBit();
         byte ReadByte();
         sbyte ReadSByte();
         void ReadBytesTo(Span<byte> output, int count);
         void ReadBytesTo(Span<byte> output);
-        bool ReadBit();
         T ReadBits<T>(uint bitCount) where T : unmanaged;
         void ReadBits(Span<byte> span, uint bitCount);
         void SkipBytes(int count);
         void SeekByte(uint position);
         void SeekBit(uint position);
-        
     }
 }
